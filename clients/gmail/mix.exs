2e8506d3cc87a56ae3aa986846059ca94b8648bf
Jeff Ching <chingor@google.com>
2018-05-18 14:35:19 -0700

    Apply dialyzer fixes to sheets api

---
+++ clients/gmail/mix.exs
@@ -0,0 +1,46 @@
+defmodule GoogleApi.Gmail.V1.Mixfile do
+  use Mix.Project
+
+  def project do
+    [app: :google_api_gmail,
+     version: "0.0.1",
+     elixir: "~> 1.4",
+     build_embedded: Mix.env == :prod,
+     start_permanent: Mix.env == :prod,
+     description: description(),
+     package: package(),
+     deps: deps(),
+     source_url: "https://github.com/GoogleCloudPlatform/elixir-google-api/tree/master/clients/FIXME"
+    ]
+  end
+
+  def application() do
+    [extra_applications: [:logger]]
+  end
+
+  defp deps() do
+    [
+      {:tesla, "~> 0.8"},
+      {:poison, ">= 1.0.0"},
+      {:ex_doc, "~> 0.16", only: :dev}
+    ]
+  end
+
+  defp description() do
+    """
+    Access Gmail mailboxes including sending user email.
+    """
+  end
+
+  defp package() do
+    [
+      files: ["lib", "mix.exs", "README*", "LICENSE"],
+      maintainers: ["Jeff Ching"],
+      licenses: ["Apache 2.0"],
+      links: %{
+        "GitHub" => "https://github.com/GoogleCloudPlatform/elixir-google-api/tree/master/clients/FIXME",
+        "Homepage" => "https://developers.google.com/gmail/api/"
+      }
+    ]
+  end
+end
